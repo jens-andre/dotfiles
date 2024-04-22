01ee145dd3d14e4e1abb7cd8306e7d288fc296e5
jens-andre <jaw@me.com>
2024-04-22 07:17:09 +0200

    Increase conform timeout

--- nvim/init.lua
+++ nvim/init.lua
@@ -390,9 +390,9 @@ require("lazy").setup({
     "stevearc/conform.nvim",
     event = { "BufReadPre", "BufNewFile" },
     opts = {
-      notify_on_error = false,
+      notify_on_error = true,
       format_on_save = {
-        timeout_ms = 500,
+        timeout_ms = 3000,
         lsp_fallback = true,
       },
       formatters_by_ft = {
